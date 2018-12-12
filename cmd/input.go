95c58227f762e52dfb9e1339c5e0bcc9182e516b
tomas <hydrogeek@outlook.com>
2018-12-12 15:12:27 +0800

    resolve home dir

--- cmd/input.go
+++ cmd/input.go
@@ -1,67 +1,91 @@
 package cmd
 
 import (
+	"bytes"
 	"encoding/base64"
+	"fmt"
 	"io"
 	"net/http"
 	"net/url"
 	"os"
+	"os/user"
+	"path/filepath"
+	"strings"
 )
 
 // GetInputContent returns the content of input
 func GetInputContent(input string) (io.Reader, error) {
-	var r io.Reader
+	var rc io.ReadCloser
 
 	u, err := url.Parse(input)
 	if u.Scheme != "" {
-		r, err = remoteList(input)
+		rc, err = remoteList(input)
 		if err != nil {
 			return nil, err
 		}
-	} else if err != nil || u.Scheme == "" {
-		r, err = localList(input)
+	} else if input != "" && u.Scheme == "" {
+		rc, err = localList(input)
 		if err != nil {
 			return nil, err
 		}
 	} else {
-		r, err = defaultList()
+		rc, err = defaultList()
 		if err != nil {
 			return nil, err
 		}
 	}
 
-	var dst io.ReadWriter
-	src := base64.NewDecoder(base64.StdEncoding, r)
+	dst := bytes.NewBufferString("")
+	src := base64.NewDecoder(base64.StdEncoding, rc)
 	_, err = io.Copy(dst, src)
 	if err != nil {
 		return nil, err
 	}
+	defer rc.Close()
 
 	return dst, nil
 }
 
-func localList(pathname string) (io.Reader, error) {
+func localList(pathname string) (io.ReadCloser, error) {
+
+	pathname = resolveHomeDir(pathname)
+
+	pathname, err := filepath.Abs(pathname)
 	f, err := os.Open(pathname)
 	if err != nil {
 		return nil, err
 	}
-	defer f.Close()
 
 	return f, nil
 }
 
-func remoteList(urlpath string) (io.Reader, error) {
+func remoteList(urlpath string) (io.ReadCloser, error) {
 	resp, err := http.Get(urlpath)
 	if err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
 
 	return resp.Body, nil
 }
 
 const gfwlist = "https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt"
 
-func defaultList() (io.Reader, error) {
+func defaultList() (io.ReadCloser, error) {
+	fmt.Println("Get default list")
 	return remoteList(gfwlist)
+}
+
+func resolveHomeDir(pathname string) string {
+	tempStr := ""
+
+	usr, _ := user.Current()
+	dir := usr.HomeDir
+
+	if pathname == "~" {
+		tempStr = dir
+	} else if strings.HasPrefix(pathname, "~/") {
+		tempStr = filepath.Join(dir, pathname[2:])
+	}
+
+	return tempStr
 }
